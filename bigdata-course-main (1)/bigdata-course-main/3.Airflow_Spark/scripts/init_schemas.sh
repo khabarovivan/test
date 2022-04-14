0a234c7d0fcfd76dd7622a2dd2b72f4505be90df
Ivan Khabarov <52312835+khabarovivan@users.noreply.github.com>
2022-04-15 02:10:51 +0300

    Delete bigdata-course-main (1) directory

--- bigdata-course-main (1)/bigdata-course-main/3.Airflow_Spark/scripts/init_schemas.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-psql -h "${DB_HOST}" -U "${POSTGRES_USER}" -p "5432" -c "create schema if not exists raw" "postgres"
-psql -h "${DB_HOST}" -U "${POSTGRES_USER}" -p "5432" -c "create schema if not exists datamart" "postgres"
