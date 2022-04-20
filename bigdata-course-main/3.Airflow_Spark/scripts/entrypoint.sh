e5c229c46ea6e556e61097696fd4c1fc5ea76cf0
Ivan Khabarov <52312835+khabarovivan@users.noreply.github.com>
2022-04-20 22:20:26 +0300

    Update entrypoint.sh

--- bigdata-course-main/3.Airflow_Spark/scripts/entrypoint.sh
+++ bigdata-course-main/3.Airflow_Spark/scripts/entrypoint.sh
@@ -3,5 +3,6 @@
 airflow db upgrade
 airflow initdb
 airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
+airflow connections --add --conn_id=google_cloud_default --conn_type=google_cloud_platform --conn_extra='{ "extra__google_cloud_platform__key_path":"/opt/airflow/credentials/seventh-oven-347013-bc0e6ba03fe0.json", "extra__google_cloud_platform__project": "seventh-oven-347013", "extra__google_cloud_platform__scope": "https://www.googleapis.com/auth/drive"}'
 
 airflow webserver
